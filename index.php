3d6cb11badb2e76c387c7243ab99ac35ee746c62
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-18 23:04:20 -0300

    Fix: fixed index php

--- index.php
+++ index.php
@@ -1,47 +1,42 @@
 <?php
-use Slim\Factory\AppFactory;
-use Slim\Middleware\ErrorMiddleware;
-use Slim\Exception\HttpNotFoundException;
+require 'vendor/autoload.php';
+require 'config/database.php';
 
-require __DIR__ . '/../vendor/autoload.php';
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$requestUri = $_SERVER["REQUEST_URI"];
 
-// Instantiate App
-$app = AppFactory::create();
+$routes = [
+    ['/', 'home'],
+    ['/users', 'user'],
+    ['/recipes', 'recipe'],
+    ['/auth', 'auth']
+];
 
-// Add error middleware
-$app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+foreach ($routes as $route) {
+    if (preg_match("#^$route[0]$#", $requestUri, $matches)) {
+        $controllerName = ucfirst($route[1]) . "Controller";
+        require "classes/$controllerName.php";
+        $controller = new $controllerName();
 
-// Get container
-$container = $app->getContainer();
+        switch ($requestMethod) {
+            case 'GET':
+                $controller->index();
+                break;
+            case 'POST':
+                $controller->create();
+                break;
+            case 'PUT':
+                $controller->update();
+                break;
+            case 'DELETE':
+                $controller->delete();
+                break;
+        }
+        break;
+    }
+}
 
-// Set up controllers
-$container->set('RecipeController', function ($container) {
-    return new RecipeController($container->get('RecipeDAO'));
-});
-
-$container->set('UserController', function ($container) {
-    return new UserController($container->get('UserDAO'));
-});
-
-$container->set('AuthController', function ($container) {
-    return new AuthController($container->get('UserDAO'));
-});
-
-// Set up middleware
-$container->set('JwtMiddleware', function ($container) {
-    return new JwtMiddleware($container->get('jwt'));
-});
-
-// Set up routes
-require __DIR__ . '/../app/routes/authRoutes.php';
-require __DIR__ . '/../app/routes/recipeRoutes.php';
-require __DIR__ . '/../app/routes/userRoutes.php';
-
-// Add route not found handler
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
-    throw new HttpNotFoundException($request);
-});
-
-$app->run();
+if (!isset($controller)) {
+    http_response_code(404);
+    echo "Endpoint não encontrado.";
+}
